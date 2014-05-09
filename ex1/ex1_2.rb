b22573c43aee899d2cdbecfebbdaff331e83737d
legion <phenixdoc@gmail.com>
2014-05-09 12:01:12 -0300

    bugfix

--- ex1/ex1_2.rb
+++ ex1/ex1_2.rb
@@ -2,11 +2,13 @@ def hello(name)
 	return "Hello, " + name
 end
 
-def starts_with_constant?(s)
+def starts_with_consonant?(s)
 	if s.length == 0
 		return false
 	end
-	return ["a", "e", "i", "o", "u"].include? s[0].downcase
+	is_cons = not(["a", "e", "i", "o", "u"].include?(s[0].downcase()))
+	is_letter = ((s[0].downcase =~ /^[a-z]$/) == 0)
+	return (is_letter and is_cons)
 end
 
 def binary_multiple_of_4? s
